16a5a1bff1e9da40d5493576de4ef17ce7f517d4
yichengh <395076316@qq.com>
2016-01-03 22:58:51 +0800

    Update photo list.

--- photo/list.js
+++ photo/list.js
@@ -1,28 +1,28 @@
 var dataStr = [];
 
 dataStr.push("\
-IMG_3777.JPG  Deng's portrait  @Litchi Park<br>\
-IMG_3781.JPG<br>\
-IMG_3797.JPG  Dameisha Beach<br>\
+IMG_5262.JPG  @Deng's portrait  Litchi Park<br>\
+IMG_3781.JPG  @Shenzhen Stock Exchange<br>\
+IMG_3797.JPG  @Dameisha Beach<br>\
 IMG_3804.JPG<br>\
 IMG_3809.JPG<br>\
 IMG_3810.JPG<br>\
 IMG_3822.JPG<br>\
-IMG_3834.JPG<br>\
+IMG_5266.JPG<br>\
 IMG_3839.JPG<br>\
 IMG_3843.JPG<br>\
 IMG_3844.JPG<br>\
 IMG_3869.JPG<br>\
-IMG_3876.JPG<br>\
-IMG_3885.JPG<br>\
+IMG_5264.JPG<br>\
+IMG_5265.JPG<br>\
 IMG_3907.JPG<br>\
 IMG_3910.JPG<br>\
 IMG_3920.JPG<br>\
 IMG_3924.JPG<br>\
 IMG_3925.JPG<br>\
 IMG_3927.JPG<br>\
-IMG_3928.JPG  Window of the world<br>\
-IMG_3929.JPG  Shenzhen Bay Park<br>\
+IMG_3928.JPG  @Window of the world<br>\
+IMG_3929.JPG  @Shenzhen Bay Park<br>\
 IMG_3936.JPG<br>\
 IMG_3940.JPG<br>\
 ");
@@ -31,19 +31,19 @@ dataStr.push("\
 IMG_3946.JPG<br>\
 IMG_3953.JPG<br>\
 IMG_3954.JPG<br>\
-IMG_4005.JPG<br>\
-IMG_4012.JPG<br>\
+IMG_4005.JPG  @Victoria Harbour<br>\
+IMG_4012.JPG  @Victoria Harbour<br>\
 IMG_4025.JPG<br>\
 IMG_4031.JPG<br>\
-IMG_4051.JPG<br>\
-IMG_4052.JPG<br>\
-IMG_4057.JPG<br>\
-IMG_4060.JPG<br>\
-IMG_4076.JPG<br>\
-IMG_4084.JPG<br>\
-IMG_4109.JPG<br>\
-IMG_4162.JPG<br>\
-IMG_4169.JPG<br>\
+IMG_4051.JPG  @Madame Tussaud's<br>\
+IMG_4052.JPG  @Madame Tussaud's<br>\
+IMG_4057.JPG  @Madame Tussaud's<br>\
+IMG_4060.JPG  @Madame Tussaud's<br>\
+IMG_4076.JPG  @Madame Tussaud's<br>\
+IMG_4084.JPG  @Madame Tussaud's<br>\
+IMG_4109.JPG  @Victoria Peak<br>\
+IMG_4162.JPG  @Victoria Peak<br>\
+IMG_5274.JPG<br>\
 IMG_4177.JPG<br>\
 IMG_4182.JPG<br>\
 IMG_4200.JPG<br>\
@@ -51,5 +51,86 @@ IMG_4210.JPG<br>\
 IMG_4216.JPG<br>\
 IMG_4232.JPG<br>\
 IMG_4247.JPG<br>\
-IMG_4258.JPG<br>\
+IMG_4258.JPG  @Avenue of Stars<br>\
+");
+
+dataStr.push("\
+IMG_4262.JPG<br>\
+IMG_4263.JPG<br>\
+IMG_4270.JPG<br>\
+IMG_4274.JPG<br>\
+IMG_4280.JPG<br>\
+IMG_4283.JPG<br>\
+IMG_4292.JPG<br>\
+IMG_4296.JPG<br>\
+IMG_4297.JPG<br>\
+IMG_4303.JPG<br>\
+IMG_4360.JPG<br>\
+IMG_4361.JPG<br>\
+IMG_4365.JPG<br>\
+IMG_4366.JPG<br>\
+IMG_4367.JPG<br>\
+IMG_4368.JPG<br>\
+IMG_4369.JPG<br>\
+IMG_4376.JPG<br>\
+IMG_4378.JPG<br>\
+IMG_4398.JPG<br>\
+IMG_4407.JPG<br>\
+IMG_4411.JPG<br>\
+IMG_5282.JPG<br>\
+IMG_4421.JPG<br>\
+IMG_4429.JPG<br>\
+IMG_4435.JPG<br>\
+IMG_4439.JPG<br>\
+IMG_4440.JPG<br>\
+IMG_4454.JPG<br>\
+IMG_4467.JPG<br>\
+");
+
+dataStr.push("\
+IMG_4632.JPG  @Peking University<br>\
+IMG_4650.JPG  @Tsinghua University<br>\
+IMG_4685.JPG  @Peking University<br>\
+IMG_4690.JPG  @Tiananmen Square<br>\
+IMG_4693.JPG<br>\
+IMG_4770.JPG<br>\
+IMG_4785.JPG<br>\
+IMG_4790.JPG<br>\
+IMG_4802.JPG<br>\
+IMG_4861.JPG<br>\
+IMG_4866.JPG  @The Place<br>\
+IMG_5238.JPG<br>\
+IMG_5239.JPG<br>\
+IMG_5240.JPG<br>\
+IMG_5241.JPG<br>\
+IMG_5242.JPG<br>\
+IMG_5244.JPG<br>\
+IMG_5245.JPG<br>\
+IMG_5246.JPG  @Beihai Park<br>\
+IMG_5247.JPG  @Jingshan Park<br>\
+IMG_5248.JPG<br>\
+IMG_5251.JPG<br>\
+IMG_5258.JPG<br>\
+IMG_5259.JPG<br>\
+");
+
+dataStr.push("\
+IMG_5003.JPG  @Huaqiang North Shopping District<br>\
+IMG_5009.JPG<br>\
+IMG_5090.JPG  @Mangrove<br>\
+IMG_5098.JPG<br>\
+IMG_5099.JPG  @Sea World<br>\
+IMG_5113.JPG<br>\
+IMG_5213.JPG<br>\
+IMG_5214.JPG<br>\
+IMG_5215.JPG<br>\
+IMG_5216.JPG<br>\
+IMG_5225.JPG<br>\
+IMG_5230.JPG<br>\
+IMG_5231.JPG<br>\
+IMG_5232.JPG<br>\
+IMG_5233.JPG<br>\
+IMG_5234.JPG<br>\
+1.JPG<br>\
+2.JPG<br>\
 ");
